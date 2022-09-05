06ee018c91ac50814bd0bd5e14e6a94afc409bc8
slashlifetw <dagancivil1931@gmail.com>
2022-09-05 21:02:59 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,15 @@
+products = []
+while True:
+    name = input('請輸入商品名稱: ')
+    if name == 'q':
+        break
+    price = input('請輸入商品價格: ')
+    # p = []
+    # p.append(name) # 小清單
+    # p.append(price) # 小清單
+    # 上面三行等於下面一行
+    # p = [name, price]
+    # products.append(p) # 大清單
+    # 上面兩行可以合併成下面一行
+    products.append([name, price])
+print(products)
