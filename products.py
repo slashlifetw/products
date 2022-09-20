5b7a6aa571ca188154d4648f00be04ac4d76fc9a
slashlifetw <dagancivil1931@gmail.com>
2022-09-20 20:11:31 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,21 @@
-products = []
+#讀取檔案
+products = [] 
+with open('products.csv', 'r') as f:
+    for line in f:
+        if '商品, 價格' in line:
+            continue
+            # continue意思是跳下一個迴圈 
+        # s = line.strip().split(',')
+        # name = s[0]
+        # price = s[1]
+        # 上面三行進化合併如下行
+        name, price = line.strip().split(',')
+        # split=切割，我們是用','去區分'商品'及'價格' 所以用','下去當切割線
+        # print(s)
+        products.append([name, price])
+print(products)
+
+# 讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
@@ -15,11 +32,12 @@ while True:
     products.append([name, price])
 print(products)
 
+# 印出所有購買紀錄
 for p in products:
     print(p[0], '的價格是', p[1])
 
-
-with open('products.csv', 'w') as f:
+# 寫入檔案
+with open('products.csv', 'w', encoding = 'utf-8') as f:
     f.write('商品, 價格\n')
     for p in products:
         f.write(p[0] + ',' + str(p[1]) + '\n')
